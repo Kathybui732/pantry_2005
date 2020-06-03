c1455f97d08ad2ba8e27fb2f7607f96c1ef574e1
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-03 11:15:43 -0600

    Create method for ingredients

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,11 +1,22 @@
 class CookBook
-  attr_reader :recipes
+  attr_reader :recipes,
+              :ingredients
 
   def initialize
     @recipes = []
+    @ingredients = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+    recipe.ingredients.each do |ingredient|
+      @ingredients << ingredient.name unless @ingredients.include?(ingredient.name)
+    end
   end
+
+  # def highest_calorie_meal
+  #   @recipes.max_by do |recipe|
+  #     recipe.total_calories
+  #   end
+  # end
 end
