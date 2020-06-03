3499ffd4389e00c873f92c6875f81f9e74195ff4
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-03 11:28:45 -0600

    Create test for enought ingredients for test

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -2,6 +2,7 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/pantry"
 require "./lib/ingredient"
+require "./lib/recipe"
 
 class  PantryTest < MiniTest::Test
 
@@ -9,6 +10,7 @@ class  PantryTest < MiniTest::Test
     @pantry = Pantry.new
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @recipe1 = Recipe.new("Mac and Cheese")
 	end
 
 	def test_it_exists_with_attributes
@@ -25,4 +27,15 @@ class  PantryTest < MiniTest::Test
     assert_equal 7, @pantry.stock_check(@ingredient2)
   end
 
+  def test_enough_ingredients_for
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+    @pantry.restock(@ingredient2, 7)
+    assert_equal false, @pantry.enough_ingredients_for?(@recipe1)
+    @pantry.restock(@ingredient2, 1)
+    assert_equal true, @pantry.enough_ingredients_for?(@recipe1)
+  end
 end
