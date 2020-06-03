00ab26c16b58e8e0fa852b049b18dbedc9590a92
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-03 10:33:40 -0600

    Finish iteration 1

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -15,4 +15,14 @@ class  PantryTest < MiniTest::Test
 		assert_instance_of Pantry, @pantry
 	end
 
+  def test_it_starts_with_no_stock_until_restock
+    assert_equal ({}), @pantry.stock
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+    @pantry.restock(@ingredient2, 7)
+    assert_equal 7, @pantry.stock_check(@ingredient2)
+  end
+
 end
