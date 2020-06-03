78846aee749dd4821e4d13bd7dd57844ddf3819c
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-03 11:39:45 -0600

    fix methods to give each method one job

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,17 +1,34 @@
 class CookBook
-  attr_reader :recipes,
-              :ingredients
+  attr_reader :recipes
+              # :ingredients
 
   def initialize
     @recipes = []
-    @ingredients = []
+    # @ingredients = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
-    recipe.ingredients.each do |ingredient|
-      @ingredients << ingredient.name unless @ingredients.include?(ingredient.name)
+    # recipe.ingredients.each do |ingredient|
+    #   @ingredients << ingredient.name unless @ingredients.include?(ingredient.name)
+    # end
+    # This gives add_recipe 2 jobs whcih we don't want
+  end
+
+  def ingredients
+    # ingredients = []
+    ingredient_objects = @recipes.flat_map do |recipe|
+      recipe.ingredients
     end
+    # ingredient_objects.each do |ingredient|
+    #   ingredients << ingredient.name unless ingredients.include?(ingredient.name)
+    # end
+    # ingredients
+    ingredient_objects.reduce([]) do |acc, ingredient|
+      acc << ingredient.name unless acc.include?(ingredient.name)
+      acc
+    end
+
   end
 
   def highest_calorie_meal
