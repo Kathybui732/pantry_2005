8ef1eb2ab539a79bb04f15216eec770eab18ab38
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-03 10:40:50 -0600

    Fix attr_reader

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,14 @@
 class Recipe
-  attr_reader :name
+  attr_reader :name,
+              :ingredients_required
 
   def initialize(name)
     @name = name
+    @ingredients_required = {}
+  end
+
+  def add_ingredient(ingredient, amount)
+    @ingredients_required[ingredient] ||= 0
+    @ingredients_required[ingredient] += amount
   end
 end
