31f56ddaeb3a43756a835cc3bad9dd7adc61fa53
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-03 11:15:34 -0600

    Create method for ingredients

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -10,12 +10,15 @@ class CookBookTest < MiniTest::Test
 	def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
     @recipe1.add_ingredient(@ingredient1, 2)
     @recipe1.add_ingredient(@ingredient1, 4)
     @recipe1.add_ingredient(@ingredient2, 8)
     @cookbook = CookBook.new
+    @pantry = Pantry.new
 	end
 
 	def test_it_exists_with_attributes
@@ -28,4 +31,16 @@ class CookBookTest < MiniTest::Test
     @cookbook.add_recipe(@recipe2)
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
   end
+
+  def test_highest_calorie_meal
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], @cookbook.ingredients
+  end
 end
+# @cookbook.highest_calorie_meal
